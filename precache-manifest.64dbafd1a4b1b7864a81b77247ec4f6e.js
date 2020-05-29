9b95032ef00e7701a85acb510b9762715c2ae2ad
eric <ericsilva328@gmail.com>
2020-05-29 16:02:52 -0300

    primeiro commit

---
+++ precache-manifest.64dbafd1a4b1b7864a81b77247ec4f6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c4bbccfb48e8b41c0c7834a4c23040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76b2ec81f066d9523ea3",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "86d3d20e8e4aee5bdce2",
+    "url": "/static/css/main.d80a124a.chunk.css"
+  },
+  {
+    "revision": "76b2ec81f066d9523ea3",
+    "url": "/static/js/2.9211332a.chunk.js"
+  },
+  {
+    "revision": "86d3d20e8e4aee5bdce2",
+    "url": "/static/js/main.81d277d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
